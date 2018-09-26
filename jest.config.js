e43fb2b9ffbd130c93029ba41ac017387d121e4b
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2018-09-26 11:07:10 -0500

    Add jest config and tsconfig changes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,18 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  "roots": [
+    "./src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  collectCoverage: true
+}
